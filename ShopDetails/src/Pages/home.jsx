0c340ca6b754162068d04511a9659196181cf3ca
Manickvel <166627683+Manickvel@users.noreply.github.com>
2025-07-27 20:14:36 +0530

    added logo

--- ShopDetails/src/Pages/home.jsx
+++ ShopDetails/src/Pages/home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
         <div className="container homecard">
             <div className="homecard">
             <h1 className="test">Product Show Case</h1>
-            <p className="text-center pb-18 pt-2 font-medium text-white con">Looking for something special? Explore our wide range of products and enjoy a seamless shopping experience – your next favorite item is just a click away!</p>
+            <p className="text-center pb-18 pt-2 font-medium text-black  font-bold con">Looking for something special? Explore our wide range of products and enjoy a seamless shopping experience – your next favorite item is just a click away!</p>
             <button className="homebutton" onClick={()=>navigate('/users')} style={{color:'#4c0082'}}> <span>Get Started ---{'>'} </span></button>
             
             </div>
